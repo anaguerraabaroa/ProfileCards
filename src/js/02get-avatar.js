b91c6a568bf64d330a47be334cd8a696c0352fd2
anaguerraabaroa <ana.guerra.abaroa@gmail.com>
2020-10-11 17:19:16 +0100

    Fixed quotes problem

--- src/js/02get-avatar.js
+++ src/js/02get-avatar.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 
 const fr = new FileReader();
-const uploadBtn = document.querySelector(".js__profile-trigger");
-const fileField = document.querySelector(".js__profile-upload-btn");
-const profileImage = document.querySelector(".js__profile-image");
-const profilePreview = document.querySelector(".js__profile-preview");
+const uploadBtn = document.querySelector('.js__profile-trigger');
+const fileField = document.querySelector('.js__profile-upload-btn');
+const profileImage = document.querySelector('.js__profile-image');
+const profilePreview = document.querySelector('.js__profile-preview');
 
 /**
  * Recoge el archivo añadido al campo de tipo "file"
@@ -16,7 +16,7 @@ const profilePreview = document.querySelector(".js__profile-preview");
  */
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener("load", writeImage);
+  fr.addEventListener('load', writeImage);
   fr.readAsDataURL(myFile);
 }
 
@@ -47,5 +47,5 @@ function fakeFileClick() {
  * - al botón visible para generar el click automático
  * - al campo oculto para cuando cambie su value
  */
-uploadBtn.addEventListener("click", fakeFileClick);
-fileField.addEventListener("change", getImage);
+uploadBtn.addEventListener('click', fakeFileClick);
+fileField.addEventListener('change', getImage);
