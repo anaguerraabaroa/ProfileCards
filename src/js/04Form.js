83c15371c03c57948d66b28e3605338bbf3c93ff
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-13 19:19:28 +0200

    Add name and position form javascript

--- src/js/04Form.js
+++ src/js/04Form.js
@@ -1,1 +1,24 @@
 "use strict";
+
+console.log("Hola mundo 2");
+
+const nameForm = document.querySelector(".js-nameForm");
+const nameCard = document.querySelector(".js-nameCard");
+const positionForm = document.querySelector(".js-positionForm");
+
+const positionCard = document.querySelector(".js-positionCard");
+const emailForm = document;
+
+function addName() {
+  let nameValue = nameForm.value;
+  nameCard.innerHTML = nameValue;
+}
+
+nameForm.addEventListener("keydown", addName);
+
+function addPosition() {
+  let positionValue = positionForm.value;
+  positionCard.innerHTML = positionValue;
+}
+
+positionForm.addEventListener("keydown", addPosition);
