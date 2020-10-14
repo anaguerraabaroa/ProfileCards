895170feb2e17b58edf4820ab7dfd5be79864704
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-14 20:27:35 +0200

    add halloween 2 palette

--- src/js/04palette.js
+++ src/js/04palette.js
@@ -1,10 +1,10 @@
 "use strict";
 
-/* const palette1 = document.querySelector(".paletteRadio1");
-const palette2 = document.querySelector(".paletteRadio2");
-const palette3 = document.querySelector(".paletteRadio3");
-const palette4 = document.querySelector(".paletteRadio4");
- */
+const palette1 = document.querySelector(".js-paletteRadio1");
+const palette2 = document.querySelector(".js-paletteRadio2");
+const palette3 = document.querySelector(".js-paletteRadio3");
+const palette4 = document.querySelector(".js-paletteRadio4");
+
 function changeColors() {}
 
 palette1.addEventListener("click", changeColors);
