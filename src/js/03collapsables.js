c6e47db2f949972a5b925e0df8976cfd5d685df5
Celiamf <celiamartinfern@gmail.com>
2020-10-15 20:35:00 +0200

    Fail attempt to solve collapsables.js

--- src/js/03collapsables.js
+++ src/js/03collapsables.js
@@ -2,14 +2,41 @@
 console.log("Hola mundo");
 
 const arrow = document.querySelectorAll(".js-button");
-const collapsable = document.querySelector(".js-mainCollapsable");
+const collapsable = document.querySelectorAll(".js-mainCollapsable");
 
-function rotateArrow(event) {
+function getCollapsable(event) {
   event.currentTarget.classList.toggle("changeArrow");
-  collapsable.classList.toggle("hideCollapsable");
-  console.log(event.currentTarget);
+  if (arrow[0] === true) {
+    collapsable[0].classList.toggle("hideCollapsable");
+    // collapsable.classList.toggle("hideCollapsable");
+    console.log(event.currentTarget);
+  }
 }
 
+// ---------- ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ESTO FUNCIONA Y NO SE QUITA!!!!!!!!!!!! --------------
+// function getCollapsable(event) {
+//   event.currentTarget.classList.toggle("changeArrow");
+//   collapsable.classList.toggle("hideCollapsable");
+//     console.log(event.currentTarget);
+//   }
+// }
+
+// for (let i = 0; i < arrow.length; i++) {
+//   arrow[i].addEventListener("click", getCollapsable);
+// }
+// ---------- ¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡¡ESTO FUNCIONA Y NO SE QUITA!!!!!!!!!!!! --------------
+
+/* function rotateArrow(event) {
+  
+  } else if(arrow[1] === true){
+    event.currentTarget.classList.toggle("changeArrow");
+    collapsable[1].classList.toggle("hideCollapsable");    
+  }else if{
+    event.currentTarget.classList.toggle("changeArrow");
+    collapsable[2].classList.toggle("hideCollapsable");
+  }
+}
+ */
 for (let i = 0; i < arrow.length; i++) {
-  arrow[i].addEventListener("click", rotateArrow);
+  arrow[i].addEventListener("click", getCollapsable);
 }
