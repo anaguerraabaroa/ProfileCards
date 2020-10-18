3a22f045989adcaec8195a9bf75ea284ce923236
Celiamf <celiamartinfern@gmail.com>
2020-10-18 12:47:25 +0200

    Attempt to fix file conflict

--- src/js/04Form.js
+++ src/js/04Form.js
@@ -1,25 +1,20 @@
 "use strict";
-
-/* console.log("Hola mundo 2");
-
 const inputList = document.querySelectorAll(".js-field"); //formulario
-const nameCard = document.querySelector(".js-nameCard"); // tarjeta
-const positionCard = document.querySelector(".js-positionCard"); // tarjeta
-const tlCard = document.querySelector(".js-tlCard"); //tarjeta
-const emailCard = document.querySelector(".js-emailCard"); //tarjeta
-const linkedinCard = document.querySelector(".js-linkedinCard"); //tarjeta
-const gitHubCard = document.querySelector(".js-gitHubCard"); //tarjeta
-
+const nameCard = document.querySelector(".js-nameCard");
+const positionCard = document.querySelector(".js-positionCard");
+// FullName
+inputList[0].addEventListener("keydown", saveField);
 function saveField(event) {
-  let fieldValue = event.currentTarget.value;
-  nameCard.innerHTML = fieldValue;
-  positionCard.innerHTML = fieldValue;
-  emailCard.innerHTML = fieldValue;
-  tlCard.innerHTML = fieldValue;
-  linkedinCard.innerHTML = fieldValue;
-  gitHubCard.innerHTML = fieldValue;
+  const nameField = event.currentTarget.value;
+  nameCard.innerHTML = nameField;
 }
-
-for (const eachItem of inputList) {
-  eachItem.addEventListener("keydown", saveField);
-} */
+// Position
+inputList[1].addEventListener("keydown", saveField2);
+function saveField2(event) {
+  const positionField = event.currentTarget.value;
+  positionCard.innerHTML = positionField;
+}
+// E-mail
+// Teléfono
+// Linkedin
+// Github
