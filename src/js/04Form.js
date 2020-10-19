9413134f77eca24da550be99807ee0202f094986
Celiamf <celiamartinfern@gmail.com>
2020-10-19 14:21:48 +0200

    Fix nav borders

--- src/js/04Form.js
+++ src/js/04Form.js
@@ -3,13 +3,13 @@ const inputList = document.querySelectorAll(".js-field"); //formulario
 const nameCard = document.querySelector(".js-nameCard");
 const positionCard = document.querySelector(".js-positionCard");
 // FullName
-inputList[0].addEventListener("keydown", saveField);
+inputList[0].addEventListener("keyup", saveField);
 function saveField(event) {
   const nameField = event.currentTarget.value;
   nameCard.innerHTML = nameField;
 }
 // Position
-inputList[1].addEventListener("keydown", saveField2);
+inputList[1].addEventListener("keyup", saveField2);
 function saveField2(event) {
   const positionField = event.currentTarget.value;
   positionCard.innerHTML = positionField;
